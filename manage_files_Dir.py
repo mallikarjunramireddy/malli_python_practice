ead6430e5ba3c8b0622efe4e71ac57e5e9492093
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-02 16:44:55 +0530

    python practice

--- manage_files_Dir.py
+++ manage_files_Dir.py
@@ -3,16 +3,16 @@ import shutil
 
 os.system("date")
 
-def list_files(directory):
-    print(f"Files in {directory}:")
-    for file_name in os.listdir(directory):
-        print(file_name)
+# def list_files(directory):
+#     print(f"Files in {directory}:")
+#     for file_name in os.listdir(directory):
+#         print(file_name)
 
-# def check_disk_usage():
-#     usage = shutil.disk_usage("/")
-#     print(f"Total: {usage.total // (1024**3)} GB")
-#     print(f"Used: {usage.used // (1024**3)} GB")
-#     print(f"Free: {usage.free // (1024**3)} GB")
+def check_disk_usage():
+    usage = shutil.disk_usage("/")
+    print(f"Total: {usage.total // (1024**3)} GB")
+    print(f"Used: {usage.used // (1024**3)} GB")
+    print(f"Free: {usage.free // (1024**3)} GB")
 
 # def archive_logs(log_directory, archive_directory):
 #     if not os.path.exists(archive_directory):
@@ -24,7 +24,7 @@ def list_files(directory):
 #             print(f"Archived: {file_name}")
 
 # Example usage
-list_files("/var/log")
-# check_disk_usage()
+# list_files("/var/log")
+check_disk_usage()
 # archive_logs("/var/log", "/var/log/archived_logs")
 
