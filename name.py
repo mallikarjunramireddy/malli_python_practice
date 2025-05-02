32b61e80a0fd41f4a6077859d90b45c23eded7fb
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-02 13:27:04 +0530

    python practice

---
+++ name.py
@@ -0,0 +1,8 @@
+# name.py
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to Python on Linux.")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
