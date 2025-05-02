d63e8cd1d1ca9e1cbd1cccb8e1d69fcf51081155
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-02 14:17:26 +0530

    python practice

--- name.py
+++ name.py
@@ -1,8 +1,8 @@
 # name.py
 
-def greet(name):
-    print(f"Hello, {name}! Welcome to Python on Linux.")
+def greet():
+    print("Hello from greet()")
 
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    greet(user_name)
+# if __name__ == "__main__":
+#     print("Running directly")
+    greet()
--- sum_fun.py
+++ sum_fun.py
@@ -2,8 +2,8 @@ def add_numbers(num1, num2):
   """
   This function takes two numbers as input and returns their sum.
   """
-#   sum_of_numbers = num1 + num2
-#   return sum_of_numbers
+  sum_of_numbers = num1 + num2
+  return sum_of_numbers
 
 # Example usage
 number1 = 10
