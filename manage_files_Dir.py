5a06df79233c58c2bc105242a598d968479a36bb
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-03 14:03:54 +0530

    python practice

--- manage_files_Dir.py
+++ manage_files_Dir.py
@@ -14,14 +14,14 @@ def check_disk_usage():
     print(f"Used: {usage.used // (1024**3)} GB")
     print(f"Free: {usage.free // (1024**3)} GB")
 
-# def archive_logs(log_directory, archive_directory):
-#     if not os.path.exists(archive_directory):
-#         os.makedirs(archive_directory)
-#     for file_name in os.listdir(log_directory):
-#         if file_name.endswith(".log"):
-#             shutil.move(os.path.join(log_directory, file_name),
-#                         os.path.join(archive_directory, file_name))
-#             print(f"Archived: {file_name}")
+def archive_logs(log_directory, archive_directory):
+    if not os.path.exists(archive_directory):
+        os.makedirs(archive_directory)
+    for file_name in os.listdir(log_directory):
+        if file_name.endswith(".log"):
+            shutil.move(os.path.join(log_directory, file_name),
+                        os.path.join(archive_directory, file_name))
+            print(f"Archived: {file_name}")
 
 # Example usage
 list_files("/var/log")
