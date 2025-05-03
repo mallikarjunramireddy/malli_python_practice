6b122094f512688c95d91150e6e36e9b0db30560
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-03 17:45:55 +0530

    python practice

--- user_add_remove.py
+++ user_add_remove.py
@@ -1,20 +1,20 @@
 import os
 import subprocess
 
-def create_user(username):
-    try:
-        subprocess.run(["sudo", "useradd", username], check=True)
-        print(f"User {username} created successfully.")
-    except subprocess.CalledProcessError:
-        print(f"Failed to create user {username}.")
-
-# def delete_user(username):
+# def create_user(username):
 #     try:
-#         subprocess.run(["sudo", "userdel", username], check=True)
-#         print(f"User {username} deleted successfully.")
+#         subprocess.run(["sudo", "useradd", username], check=True)
+#         print(f"User {username} created successfully.")
 #     except subprocess.CalledProcessError:
-#         print(f"Failed to delete user {username}.")
+#         print(f"Failed to create user {username}.")
+
+def delete_user(username):
+    try:
+        subprocess.run(["sudo", "userdel", username], check=True)
+        print(f"User {username} deleted successfully.")
+    except subprocess.CalledProcessError:
+        print(f"Failed to delete user {username}.")
 
 # Example usage
-create_user("testuser")
-# delete_user("testuser")
+# create_user("testuser")
+delete_user("testuser")
