ac56ef3f9fdafe8199621d1dcb501497ea4d8181
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-05-03 19:47:21 +0530

    python practice

--- cronjob.py
+++ cronjob.py
@@ -2,10 +2,10 @@ import os
 
 def add_cron_job(job_command, schedule):
     cron_job = f"{schedule} {job_command}\n"
-    with open("/tmp/cron_job", "w") as file:
+    with open("cron_job", "w") as file:
         file.write(cron_job)
-    os.system("crontab /tmp/cron_job")
+    os.system("crontab cron_job")
     print(f"Cron job added: {cron_job}")
 
 # Example usage
-add_cron_job("python3 /home/ec2-user/malli_python_practice/cronjob.py", "* * * * *")  # Run daily at 3 AM
+add_cron_job("python3 /home/ec2-user/malli_python_practice/name.py", "* * * * *")  # Run daily at 3 AM
